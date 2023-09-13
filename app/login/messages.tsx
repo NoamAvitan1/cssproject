f01282117dbc31f72b70d4024b96abd32c59a82d
Noam Avitan <noamavitan159753@gmail.com>
2023-09-13 15:49:48 +0300

    login page edit

--- app/login/messages.tsx
+++ app/login/messages.tsx
@@ -9,12 +9,12 @@ export default function Messages() {
   return (
     <>
       {error && (
-        <p className="mt-4 p-4 bg-neutral-900 text-neutral-300 text-center">
+        <p className="mt-4 p-4 bg-error text-white text-center">
           {error}
         </p>
       )}
       {message && (
-        <p className="mt-4 p-4 bg-neutral-900 text-neutral-300 text-center">
+        <p className="mt-4 p-4 bg-success text-white text-center">
           {message}
         </p>
       )}
--- app/login/page.tsx
+++ app/login/page.tsx
@@ -1,8 +1,12 @@
-import Link from 'next/link'
-import Messages from './messages'
-import { ProviderSignInButton } from '../_components/ProviderSignInButton'
+"use client";
+import Link from "next/link";
+import Messages from "./messages";
+import { ProviderSignInButton } from "../_components/ProviderSignInButton";
+import * as MaterialDesign from "react-icons/ai";
+import { useState } from "react";
 
 export default function Login() {
+  const [showPassword, setShowPassord] = useState<boolean>(false);
   return (
     <div className="flex-1 flex flex-col w-full px-8 sm:max-w-md justify-center gap-2">
       <Link
@@ -22,12 +26,17 @@ export default function Login() {
           className="mr-2 h-4 w-4 transition-transform group-hover:-translate-x-1"
         >
           <polyline points="15 18 9 12 15 6" />
-        </svg>{' '}
+        </svg>{" "}
         Back
       </Link>
 
-      <section className='flex flex-col gap-4'>
+      <section className="flex flex-col gap-4">
         <ProviderSignInButton />
+        <div className="text-slate-500 flex min-[420px]:justify-between justify-center items-center w-full">
+          <span className="min-[420px]:block hidden w-[75px] h-[2px] bg-slate-500"></span>
+          <span>Or with email and password</span>
+          <span className="min-[420px]:block hidden w-[75px] h-[2px] bg-slate-500"></span>
+        </div>
         <form
           className="flex-1 flex flex-col w-full justify-center gap-2 text-foreground"
           action="/auth/sign-in"
@@ -36,23 +45,39 @@ export default function Login() {
           <label className="text-md" htmlFor="email">
             Email
           </label>
-          <input
-            className="rounded-md px-4 py-2 bg-inherit border mb-6"
-            name="email"
-            placeholder="you@example.com"
-            required
-          />
+          <section className="rounded-md border mb-6 flex justify-between items-center relative">
+            <input
+              className="p-2 w-full"
+              name="email"
+              placeholder="you@example.com"
+              required
+            />
+            <span className="absolute right-2.5">{<MaterialDesign.AiOutlineMail/>}</span>
+          </section>
           <label className="text-md" htmlFor="password">
             Password
           </label>
-          <input
-            className="rounded-md px-4 py-2 bg-inherit border mb-6"
-            type="password"
-            name="password"
-            placeholder="••••••••"
-            required
-          />
-          <button className="bg-green-700 rounded px-4 py-2 text-white mb-2">
+          <section className="rounded-md border mb-6 flex justify-between items-center relative">
+            <input
+              className="p-2 w-full"
+              type={showPassword ? "text" : "password"}
+              name="password"
+              placeholder="••••••••"
+              required
+            />
+            <span className="absolute right-2.5 cursor-pointer">
+              {showPassword ? (
+                <MaterialDesign.AiOutlineEye
+                  onClick={() => setShowPassord(false)}
+                />
+              ) : (
+                <MaterialDesign.AiOutlineEyeInvisible
+                  onClick={() => setShowPassord(true)}
+                />
+              )}{" "}
+            </span>
+          </section>
+          <button className="bg-accent rounded px-4 py-2 text-white mb-2">
             Sign In
           </button>
           <button
@@ -65,5 +90,5 @@ export default function Login() {
         </form>
       </section>
     </div>
-  )
+  );
 }
