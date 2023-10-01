fd3510aa6326e91d7341c28d1c82d7ff81062a90
Noam Avitan <noamavitan159753@gmail.com>
2023-10-01 12:50:59 +0300

    fix problems

--- app/_module/MenuModule.tsx
+++ app/_module/MenuModule.tsx
@@ -1,5 +1,6 @@
 'use client'
 import {GiHamburgerMenu} from 'react-icons/gi'
+import {AiOutlineClose} from 'react-icons/ai'
 
 import { Database } from "@/types/supabase";
 import { useState } from "react";
@@ -14,8 +15,12 @@ export const MenuModule = (props: Props) => {
   const [toggle,setToggle] = useState<boolean>(false);
 
   return (
-    <div className={`${toggle ? 'bg-red-600' :''}`} onClick={()=>setToggle(!toggle)}>
-        <GiHamburgerMenu/>
+    <div className=''>
+      <button className='z-10 absolute right-2 top-4 text-text' onClick={()=>setToggle(!toggle)}>{toggle ?<AiOutlineClose/> :<GiHamburgerMenu/> }</button>
+      {toggle ?<div className={`absolute bg-black w-full h-full top-0 bg-opacity-30 rounded-md right-full duration-200  ${toggle ?'translate-x-full':''}`}>
+
+      </div> :''}
     </div>
   );
 };
+
--- app/_module/Modules.tsx
+++ app/_module/Modules.tsx
@@ -1,7 +1,7 @@
 import {GiHamburgerMenu} from 'react-icons/gi'
 import { cookies } from "next/headers";
 import { createServerComponentClient } from "@supabase/auth-helpers-nextjs";
-import {MenuModule} from './MenuModule'
+import { MenuModule } from './MenuModule';
 type Props = {};
 
 export default async function Modules(props: Props) {
@@ -9,18 +9,16 @@ export default async function Modules(props: Props) {
         const { data: modules, error } = await supabase.from("module").select("*");
 
     return (
-        <div className='w-10/12 space-y-6 '>
-            <h1 className="border-b-2 text-xl w-full sm:text-2xl">Popular Modules to Check Out</h1>
+        <div className='w-10/12 space-y-6'>
+            <h1 className="border-b-2 text-2xl w-full">Popular Modules to Check Out</h1>
             <section className="grid xl:grid-cols-4 lg:grid-cols-3 sm:grid-cols-2 gap-10">
               {modules ? modules.map((v, i) => (
-                <article className="bg-secondary p-2 rounded-md aspect-square" key={i}>
+                <article className="bg-secondary p-2 rounded-md aspect-square relative" key={i}>
                    <header className="w-full flex justify-between items-center">
                        <p className="text-xl">
                             {v.title}
                         </p>
-                        <button className="text-xl">
-                        <MenuModule modules= {v}/>
-                        </button>
+                        <MenuModule modules = {v}/>
                    </header>
                 </article>
               )) : null}
