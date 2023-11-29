aec42dd91865ee874f7020ad33a5a37333300002
Noam Avitan <noamavitan159753@gmail.com>
2023-11-29 17:01:01 +0200

    route update

--- app/_components/profile/UserData.tsx
+++ app/_components/profile/UserData.tsx
@@ -37,18 +37,20 @@ export const UserData = (props: Props) => {
            <section className="flex md:flex-row flex-col gap-5 xl:w-1/4 lg:w-1/3">
              <img className="rounded-md" src="https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460_960_720.png" alt="" />
            </section>
-           <section className="flex flex-col lg:w-2/3 xl:w-3/4 justify-between gap-3">
+           <section className="flex flex-col lg:w-2/3 xl:w-3/4 gap-5">
             <div className="flex justify-between">
-              <section className="flex flex-col gap-3">
-                <p className="border border-primary p-1 rounded-md w-fit">{user[0]?.user_name}</p>
-                <p className="border border-primary p-1 rounded-md">{user[0]?.email}</p>
+              <section className="flex flex-col gap-3 [&_*]:w-fit [&_*]:border border-primary [&_*]:p-1 [&_*]:rounded-md">
+                <p className="">{user[0]?.user_name}</p>
+                <p className="">{user[0]?.email}</p>
               </section>
               <section>
                 <MdOutlineEdit  className="text-text text-4xl p-2 cursor-pointer bg-secondary rounded-full -mt-1"/>
               </section>
             </div>
-            <div className="w-full bg-secondary rounded-md p-2 xl:text-[17px] text-sm">
-              <p>{user[0]?.about}</p>
+            <div className="flex items-center justify-center h-full">
+              <div className="w-full bg-secondary rounded-md p-2 xl:text-[17px] text-sm">
+                <p>{user[0]?.about}</p>
+              </div>
             </div>
            </section>
         </article>
