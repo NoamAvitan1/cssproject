d4d7feffeb4e1777ee3dbf670abc2c80071a31bb
Noam Avitan <noamavitan159753@gmail.com>
2023-11-29 16:19:48 +0200

    Profile update

--- app/_components/profile/UserData.tsx
+++ app/_components/profile/UserData.tsx
@@ -33,10 +33,23 @@ export const UserData = (props: Props) => {
     <div className="w-full mt-6">
       {user &&
       <main className="container border border-secondary">
-        <article className="flex flex-row items-start p-2 relative justify-between">
-           <section className="flex md:flex-row flex-col gap-5 w-full">
-             <img className="md:w-2/3 lg:w-1/4 w-full rounded-md" src="https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460_960_720.png" alt="" />
-                <MdOutlineEdit  className="absolute right-3 z-10 top-4 text-text text-xl cursor-pointer"/>
+        <article className="flex flex-col lg:flex-row p-2 gap-5">
+           <section className="flex md:flex-row flex-col gap-5 xl:w-1/4 lg:w-1/3">
+             <img className="rounded-md" src="https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460_960_720.png" alt="" />
+           </section>
+           <section className="flex flex-col lg:w-2/3 xl:w-3/4 justify-between gap-3">
+            <div className="flex justify-between">
+              <section className="flex flex-col gap-3">
+                <p className="border border-primary p-1 rounded-md w-fit">{user[0]?.user_name}</p>
+                <p className="border border-primary p-1 rounded-md">{user[0]?.email}</p>
+              </section>
+              <section>
+                <MdOutlineEdit  className="text-text text-4xl p-2 cursor-pointer bg-secondary rounded-full -mt-1"/>
+              </section>
+            </div>
+            <div className="w-full bg-secondary rounded-md p-2 xl:text-[17px] text-sm">
+              <p>{user[0]?.about}</p>
+            </div>
            </section>
         </article>
       </main>
@@ -46,5 +59,3 @@ export const UserData = (props: Props) => {
 };
 
 
-// <section className="w-3/5 flex flex-col p-2 justify-center items-center relative border border-text rounded">
-// </section>
