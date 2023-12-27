9c6c3ece699b1fce62d38e51c91dedfca8c019f8
leeskies <lee2dskies@gmail.com>
2023-12-27 20:25:55 +0200

    home page h1's design

--- app/_components/search/ModulesSearch.tsx
+++ app/_components/search/ModulesSearch.tsx
@@ -41,7 +41,7 @@ export const ModulesSearch = (props: Props) => {
   const updateQueryParam = (key: string, value: string) => {
     const currentParams = new URLSearchParams(searchParams.toString());
     currentParams.set(key, value);
-    router.replace(`?${currentParams.toString()}`, { scroll: false });
+    router.push(`?${currentParams.toString()}`);
   };
 
   return (
--- app/_module/ModulesHomePage.tsx
+++ app/_module/ModulesHomePage.tsx
@@ -1,7 +1,5 @@
 import { cookies } from "next/headers";
-import {
-  createServerComponentClient,
-} from "@supabase/auth-helpers-nextjs";
+import { createServerComponentClient } from "@supabase/auth-helpers-nextjs";
 import { ModulesData } from "./ModulesData";
 
 type Props = {};
@@ -12,10 +10,14 @@ export default async function ModulesHomePage(props: Props) {
   const query = async (type: string = "") => {
     const supabaseQuery = supabase
       .from("module")
-      .select('examples_count,access_type,created_at,description,downloads,id,price,title,title_description,user_id(id,user_name,profile_pic)')
-      .neq('access_type','private');
+      .select(
+        "examples_count,access_type,created_at,description,downloads,id,price,title,title_description,user_id(id,user_name,profile_pic)",
+      )
+      .neq("access_type", "private");
     if (type !== "") {
-      supabaseQuery.eq("access_type", type).order('downloads', { ascending: false })
+      supabaseQuery
+        .eq("access_type", type)
+        .order("downloads", { ascending: false });
     }
     const { data } = await supabaseQuery.range(0, 100).limit(3);
     return data;
@@ -26,12 +28,12 @@ export default async function ModulesHomePage(props: Props) {
   const [free, paid, random] = await Promise.all(promises);
 
   return (
-    <div className="w-10/12">
-      <h1>public modules</h1>
+    <div className="w-10/12 [&_h1]:border-b [&_h1]:border-text [&_h1]:p-2 space-y-6 [&_h1]:text-lg">
+      <h1>Public modules</h1>
       <ModulesData modules={free} />
-      <h1>paid modules</h1>
+      <h1>Paid modules</h1>
       <ModulesData modules={paid} />
-      <h1>global modules</h1>
+      <h1>Global modules</h1>
       <ModulesData modules={random} />
     </div>
   );
