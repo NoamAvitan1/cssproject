7b91e2bfb5493b6f71defeaa1d792a0fc18746b7
leeskies <lee2dskies@gmail.com>
2023-12-27 22:14:00 +0200

    removed rerouting on sign up

--- app/loading.tsx
+++ app/loading.tsx
@@ -9,7 +9,7 @@ export default function Loading (props: Props) {
 
   return (
     <div className="fixed inset-0 flex items-center justify-center backdrop-blur">
-      <div className="h-[150px] md:h-[250px] lg:h-[350px] xl:h-[450px] aspect-square rounded-full animate-spin border-t border-t-blue-500">
+      <div className="h-[150px] md:h-[250px] lg:h-[350px] aspect-square rounded-full animate-spin border-t border-t-blue-500">
 
       </div>
     </div>
